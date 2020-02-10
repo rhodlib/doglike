09df3e27d9bf1338dc59849d292b357472ce709e
Rodolfo Talibs <rhodlib@gmail.com>
2020-02-10 11:12:27 -0300

    Updates

---
+++ precache-manifest.c8a0ffc954ed8bb93a6e382d621b6e83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5f00a519f0d95512a5c86c584e248c",
+    "url": "/doglike/index.html"
+  },
+  {
+    "revision": "39d8a87f31b0b7ddc8d8",
+    "url": "/doglike/static/css/main.445381e9.chunk.css"
+  },
+  {
+    "revision": "87b6c03a257b0fad60a2",
+    "url": "/doglike/static/js/2.be794f69.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/doglike/static/js/2.be794f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d8a87f31b0b7ddc8d8",
+    "url": "/doglike/static/js/main.cac582f5.chunk.js"
+  },
+  {
+    "revision": "4cc8103c935d1b3cee71",
+    "url": "/doglike/static/js/runtime-main.8d9cc716.js"
+  },
+  {
+    "revision": "930752679e3ab4be91e76ebe5fe7a399",
+    "url": "/doglike/static/media/blackpaw.93075267.png"
+  }
+]);
